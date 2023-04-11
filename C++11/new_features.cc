d1509071971161b2157fab5b22dc03fc55fe126c
ChunGang5 <1330431376@qq.com>
2023-04-11 20:25:28 +0800

    增加makefile

--- C++11/new_features.cc
+++ C++11/new_features.cc
@@ -4,8 +4,8 @@
  * @copyright Copyrightbelongs to the individual，All Rights Reserved.
  * @author Gang Chen (smilechengang@qq.com)
  * @date 2023-03-29 20:54:54
- * @last_author:
- * @last_edit_time:
+ * @last_author: Chen Gang (cheng_caft@yusur.tech)
+ * @last_edit_time: 2023-03-30 10:30:40
  */
 
 #include <iostream>
@@ -16,12 +16,12 @@ using namespace std;
 int main()
 {
     // // 字符串原始字面量
-    // string str1 = "D:\hode\test";
-    // string str2 = R"(D:\hode\test)";
-    // string str3 = R"test(D:\hode\test)test";
-    // cout << "str1 = " << str1 << endl;
-    // cout << "str2 = " << str2 << endl;
-    // cout << "str3 = " << str3 << endl;
+    string str1 = "D:\hode\test";
+    string str2 = R"(D:\hode\test)";
+    string str3 = R"test(D:\hode\test)test";
+    cout << "str1 = " << str1 << endl;
+    cout << "str2 = " << str2 << endl;
+    cout << "str3 = " << str3 << endl;
 
     // nullptr
     // char *ptr1 = "ptr";
